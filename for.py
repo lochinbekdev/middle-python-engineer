01fa5be2d113e7249151f97231aac713c1d864f2
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2023-06-05 15:01:29 +0500

    for loop with tuple string list

--- for.py
+++ for.py
@@ -1,2 +1,17 @@
 for number in range(1, 10):
     print( number * ".",number, 'Attempt', number , number * ".")
+
+
+
+
+message=("hello world")
+
+for sequence in message:
+    print(sequence)
+
+
+
+example="Hello world"
+
+for item in example:
+    print(item)
