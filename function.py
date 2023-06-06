753ecb7f06d478925b6235e2b07008f502de9feb
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2023-06-06 09:37:04 +0500

    property and argumant

---
+++ function.py
@@ -0,0 +1,19 @@
+# def greet(first_name, last_name):
+#     print(f"Hi  {first_name} {last_name}")
+#     print("Welcome to abroad")
+
+
+# greet("Lochinbek","Abduvoitov")
+
+
+def greet(name):
+    print(f"{name}")
+
+def get_greet(name):
+    return print(f"name")
+
+message=get_greet("Lochin")
+file=open("content.txt","w")
+file.write(message)
+
+print(message)
