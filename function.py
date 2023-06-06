2a08aa4b390138d3a27897dbead3f17856806111
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2023-06-06 10:06:44 +0500

    multply function

--- function.py
+++ function.py
@@ -6,14 +6,18 @@
 # greet("Lochinbek","Abduvoitov")
 
 
-def greet(name):
-    print(f"{name}")
+# def greet(name):
+#     print(f"{name}")
 
-def get_greet(name):
-    return print(f"name")
+# def get_greet(name):
+#     return print(f"name")
 
-message=get_greet("Lochin")
-file=open("content.txt","w")
-file.write(message)
+# message=get_greet("Lochin")
+# file=open("content.txt","w")
+# file.write(message)
 
-print(message)
+# print(message)
+
+
+
+
---
+++ multply.py
@@ -0,0 +1,16 @@
+# def multiply(*numbers):
+#     total=1
+#     for number in numbers:
+#         total*=number
+#     return total
+
+
+# print(multiply(2,3,4,5))
+
+
+def save_user(**user):
+    print(user)
+
+
+save_user(id=1,name="Lochin",age=23)
+
