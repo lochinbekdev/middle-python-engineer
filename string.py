6011f52f64df3d01268ee0ca6d74a2bce2031f7d
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2023-06-13 11:02:15 +0500

    escape sequence

---
+++ string.py
@@ -0,0 +1,28 @@
+# Python string some methods
+
+course="Python Programming"
+print(len(course))
+print(course[0])
+print(course[-1])
+print(course[0:6])
+print(course[0:])
+print(course[:8])
+
+
+# Python Escape sequences
+# \"
+# \'
+# \\
+# \n
+
+subject="Python Course"
+escape="Python \" Course"
+newline="Python \n Course"
+
+print(subject)
+print(escape)
+print(newline)
+
+
+# Formated stiring
+
