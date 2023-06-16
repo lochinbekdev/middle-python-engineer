3ac75ab1571df512125c3f171a3719128902d303
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2023-06-16 16:11:36 +0500

    Offer model and migration

--- products/models.py
+++ products/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
+
 class Products(models.Model):
     name = models.CharField(max_length=255)
     price = models.FloatField()
     stock=models.IntegerField()
     image_url=models.CharField(max_length=2890)
+
+
+class Offer(models.Model):
+    code = models.CharField(max_length=255)
+    desciption = models.CharField(max_length=522)
+    discount = models.FloatField()
+
+
