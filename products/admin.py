6893f18a7b3ebc22dc3030bfb7be4f47b496c43b
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2023-06-17 14:49:02 +0500

    admin panel install

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Products
 
-# Register your models here.
+
+class ProductModel(admin.ModelAdmin):
+    list_display=('name','price','stock')
+
+
+admin.site.register(Products,ProductModel)
