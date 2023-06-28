9f1eefd713602a566ee8c0b74f4547eb91974473
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2023-06-28 17:09:53 +0500

    28-dars

---
+++ anvar/28-lesson.py
@@ -0,0 +1,12 @@
+# Class and Object
+
+
+class Students:
+    def __init__(self,name,lastname,birth):
+        self.name = name
+        self.lastname = lastname
+        self.birth = birth
+
+
+student1=Students("Alex","Sandres",1000)
+print(student1)
