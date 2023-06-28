fb00760e2cbd879afea57c7effe0b9039c462799
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2023-06-28 18:11:42 +0500

    create get_student_info method

--- anvar/29-lesson.py
+++ anvar/29-lesson.py
@@ -24,8 +24,11 @@ class Subject():
     def add_student(self,student):
         self.students.append(student)
         self.student_count += 1
+    
+    def get_student_count(self):
+        return self.student_count
 
-    def get_students(self):
+    def get_students_info(self):
         return [x.get_info() for x in self.students]
 
 
@@ -33,8 +36,10 @@ matem=Subject("Matematika")
 student1=Students("Lochin","Abduvoitov")
 student2=Students("Alex","Sandres")
 student3=Students("Husan","Anorboyev")
+student4=Students("Hasan","Anorboyev")
 matem.add_student(student1)
 matem.add_student(student2)
 matem.add_student(student3)
-print(matem.student_count)
-print(matem.get_students())
+matem.add_student(student4)
+print(matem.get_student_count())
+print(matem.get_students_info())
