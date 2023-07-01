55a3dfbdc5ac5c8a1f32700a8713c70ebc849f02
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2023-07-01 10:04:28 +0500

    Address classes

--- anvar/30-lesson.py
+++ anvar/30-lesson.py
@@ -19,6 +19,44 @@ class Person:
         return year - self.birth
     
 
-inson = Person("Lochinbek","Abduvoitov","AB3444323",2000)
+inson = Person("Alex","Sandres","AB3444323",2000)
 
-print(inson.get_age(2023))
+
+class Student(Person):
+    def __init__(self, name, lastname, passport, birth,idnumber,address):
+        super().__init__(name, lastname, passport, birth)
+        self.idnumber = idnumber
+        self.course = 1
+        self.address = address
+
+
+    def get_id(self):
+        return self.idnumber
+    
+
+    def get_course(self):
+        return self.course
+    
+
+    def get_info(self):
+        info = f"{self.name} {self.lastname} \n " 
+        info += f"Id: {self.idnumber}, Course: {self.course}"
+        return info
+    
+
+class Address:
+    def  __init__(self,home,street,region,country):
+        self.home = home
+        self.street = street
+        self.region = region 
+        self.country = country
+
+    def get_address(self):
+        return f"{self.country} davlati, {self.region} tumani, {self.street} ko'chasi , {self.home} raqamli uy"
+
+
+
+address=Address(32,"Ittifoq","Zaamin","Uzbekistan")
+talaba = Student("Alex","Sandres","AB3444323",1923,"N1234567",address)
+
+print(talaba.address.get_address())
