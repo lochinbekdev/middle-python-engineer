d8cd32be4f0d3ad738f764b692460e8fb2ba7b38
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2023-07-03 10:39:48 +0500

    dunder methods

--- anvar/32-lesson.py
+++ anvar/32-lesson.py
@@ -1,90 +1,28 @@
-#32-lesson. DUNDER METODLAR
+from uuid import uuid4
 
 
+class Transport:
 
-class Avto:
-    __num_avto = 0
-    """Avtomobil klassi"""
+    __num_transport = 0
 
-    def __init__(self, make, model, rang, yil, narh):
-        """Avtomobilning xususiyatlari"""
+    def __init__(self,make,model,color,year,cost,km):
         self.make = make
         self.model = model
-        self.rang = rang
-        self.yil = yil
-        self.narh = narh
-        Avto.__num_avto += 1
+        self.color = color
+        self.year = year
+        self.cost = cost
+        self.__km = km
+        self.__id = uuid4()
+        Transport.__num_transport += 1
 
-    @classmethod
-    def get_num_avto(cls):
-        return cls.__num_avto
+    
+    def __repr__(self) -> str:
+        return f"{self.make} tomonidan yasalgan va modeli {self.model} narhi {self.cost}"
+    
 
-    def __str__(self):
-        """Obyekt haqida ma'lumot"""
-        return f"Avto: {self.make} {self.model}. {self.narh}$"
+    def __eq__(self,y) -> bool:
+        return self.cost == y.cost
 
-    def __repr__(self):
-        """Obyekt haqida ma'lumot"""
-        return f"Avto: {self.make} {self.model}. {self.narh}$"
-
-    def __eq__(self, boshqa_avto):
-        return self.narh == boshqa_avto.narh
-
-    def __lt__(self, boshqa_avto):
-        return self.narh < boshqa_avto.narh
-
-    def __le__(self, boshqa_avto):
-        return self.narh <= boshqa_avto.narh
-
-    def get_info(self):
-        return (
-            f"{self.rang} {self.make} {self.model}.{self.yil}-yil. Narhi:{self.narh}$"
-        )
-
-
-class AvtoSalon:
-    """Avtosalon klassi"""
-
-    def __init__(self, name):
-        self.name = name
-        self.avtolar = []
-
-    def __repr__(self):
-        return f"{self.name} avtosaloni"
-
-    def __len__(self):
-        return len(self.avtolar)
-
-    def __getitem__(self, index):
-        return self.avtolar[index]
-
-    def __setitem__(self, index, value):
-        if isinstance(value, Avto):
-            self.avtolar[index] = value
-
-    def __add__(self, qiymat):
-        if isinstance(qiymat, AvtoSalon):
-            yangi_salon = AvtoSalon(f"{self.name} {qiymat.name}")
-            yangi_salon.avtolar = self.avtolar + qiymat.avtolar
-            return yangi_salon
-        elif isinstance(qiymat, Avto):
-            self.add_avto(qiymat)
-        else:
-            print(f"AvtoSalon ga {type(qiymat)} qo`shib bo`lmaydi")
-
-    def __call__(self, *param):
-        if param:
-            for avto in param:
-                self.add_avto(avto)
-        else:
-            return [avto for avto in self.avtolar]
-
-    def add_avto(self, *qiymat):
-        for avto in qiymat:
-            if isinstance(avto, Avto):
-                self.avtolar.append(avto)
-            else:
-                print("Avto obyketini kiriting")
-
-    def get_list(self):
-        return [avto for avto in self.avtolar]
+transport1=Transport("GM","Lacetti","black",2012,"$234",1000)
+transport2=Transport("GM","Lacetti","black",2012,"$234",1000)
+print(transport1==transport2)
