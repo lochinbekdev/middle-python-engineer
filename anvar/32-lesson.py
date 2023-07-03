43547126ff826bd1238340de73a14a09671f085d
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2023-07-03 11:27:19 +0500

    dunder methods in python

--- anvar/32-lesson.py
+++ anvar/32-lesson.py
@@ -17,12 +17,51 @@ class Transport:
 
     
     def __repr__(self) -> str:
-        return f"{self.make} tomonidan yasalgan va modeli {self.model} narhi {self.cost}"
+        return f"Tarnsport: {self.model}"
     
 
     def __eq__(self,y) -> bool:
         return self.cost == y.cost
+    
+    def __lt__(self,y):
+        return self.cost < y.cost
+    
+    def __le__(self,y):
+        return self.cost >= y.cost
 
+
+# print(transport1  <=  transport2)
+
+
+class TranportShop():
+    def __init__(self,name) -> None:
+        self.name = name
+        self.avtolar = []
+
+    def __getitem__(self,index):
+        return self.avtolar[index]
+    
+    def __setitem__(self,index,value):
+        self.avtolar[index] = value
+
+    def __repr__(self) -> str:
+        return f"{self.name} avtosaloni"
+    
+    def add_avto(self,*value):
+        for item in value:
+            if isinstance(item,Transport):
+                self.avtolar.append(item)
+            else:
+                print("Avto kiritilmagan")
+    
 transport1=Transport("GM","Lacetti","black",2012,"$234",1000)
-transport2=Transport("GM","Lacetti","black",2012,"$234",1000)
-print(transport1==transport2)
+transport2=Transport("GM","Toyatta","black",2012,"$244",2000)
+
+
+tranportShop1=TranportShop("Zomin")
+# tranportShop1[0]=Transport("BMW","7x","white",1212,"$2121",1212)
+tranportShop1.add_avto(transport1,transport2)
+
+
+
+print(tranportShop1[:])
