f31092ed84b4111670a6dfc400f84720278f4372
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2023-07-06 10:16:20 +0500

    file methods

--- anvar/33-lesson.py
+++ anvar/33-lesson.py
@@ -18,4 +18,21 @@ with open('data/students.txt') as file:
 with open('data/students.txt') as line:
     students=line.readlines()
     result=[student.rstrip() for student in students]
+    # print(result)
+
+#  Yangi fayl yaratadi agar bor bo'lsa o'chirib yuboradi va yangilaydi
+filename="data/new_file.txt"
+name="Xakimov Alibek"
+
+with open(filename,'w') as file:
+    result=file.write(name)
+    # print(result)
+
+
+# Mavjud fayl bo'lsa qo'shadi yoq bo'lsa yaratadi
+filename="data/new_file.txt"
+name="Xakimov Alibek Hacker bek"
+
+with open(filename,'a') as file:
+    result=file.write('\n' + name + '\n')
     print(result)
