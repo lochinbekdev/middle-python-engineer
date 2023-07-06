554872cb02f5a1c2c9238e937ed660b8b614757c
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2023-07-06 09:59:30 +0500

    file new methods

--- anvar/33-lesson.py
+++ anvar/33-lesson.py
@@ -1,11 +1,21 @@
 # Work with files
 
-with open('./pi.txt') as file:
+with open('data/pi.txt') as file:
     pi=file.read()
-print(type(pi))
+# print(type(pi))
 
 
 pi=pi.rstrip()
 pi=pi.replace('\n','')
 pi=float(pi)
-print(type(pi))
+# print(type(pi))
+
+
+with open('data/students.txt') as file:
+    for talaba in file:
+        result=talaba
+
+with open('data/students.txt') as line:
+    students=line.readlines()
+    result=[student.rstrip() for student in students]
+    print(result)
