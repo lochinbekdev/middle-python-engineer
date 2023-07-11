d9a2189f6ba3c61fa22fa67621982afb59356a03
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2023-07-11 16:00:55 +0500

    json 34-lessons

--- anvar/34-lesson.py
+++ anvar/34-lesson.py
@@ -3,7 +3,27 @@
 import json
 
 x=10 
-
 x_json=json.dumps(x)
-print(type(x_json))
-print(type(x))
+ 
+
+number=(12,23,45,46)
+number_json=json.dumps(number)
+
+# print(type(number_json))
+
+
+ill =  {
+    "name" : "Alex Sandres",
+    "age" : 30,
+    "familiy" : True,
+    "child" : ("Azle","Xazr"),
+    "allergiya" : None,
+    "hub" : [
+        {"name": "Analgin","count":0.53},
+        {"name": "Panadol","count":0.53}
+    ]
+}
+
+ill_json=json.dumps(ill, indent=4)
+print(ill_json)
+print(ill.keys())
