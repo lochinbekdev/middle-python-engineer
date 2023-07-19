7fba299672e9bd02106e4617a1b9d1c3a0b7158a
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2023-07-19 10:18:51 +0500

    test lesson

--- anvar/unittest.py
+++ anvar/unittest.py
@@ -1,10 +1,13 @@
 import unittest
-from anvar.test import get_full_name
 
 
-class NameTest(unittest.TestCase):
+from lessontest import get_full_name
+
+
+class NameTest(unittest.TestCase()):
     def test_full_name(self):
         name = get_full_name('lochinbek','abduvoitov')
         self.assertEqual(name,"Lochinbek Abduvoitov")
+
 
 unittest.main()
